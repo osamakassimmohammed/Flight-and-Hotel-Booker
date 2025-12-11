51ff34914270a0681a160e2e5d73f27349527e21
Osama Kassim <osema.kassim@gmail.com>
2025-12-11 14:50:06 +0300

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMDc0OQ/spectral.js?branch_slug=main&token=b8503fcf-0342-4291-9c21-9e8cef42857d";
+export default { extends: ruleset };
